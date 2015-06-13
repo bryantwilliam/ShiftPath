9401007b50fae36ed91448c2f1ae826c356dbaa5
gogobebe2 <gogobebe2@gmail.com>
2015-06-13 12:33:30 +1200

    1.0.1-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftpath/ShiftPath.java
+++ src/main/java/com/gmail/gogobebe2/shiftpath/ShiftPath.java
@@ -1,8 +1,16 @@
 package com.gmail.gogobebe2.shiftpath;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-    public class ShiftPath extends JavaPlugin {
+import java.util.List;
+
+public class ShiftPath extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Starting up ShiftPath. If you need me to update this plugin, email at gogobebe2@gmail.com");
@@ -11,5 +19,21 @@ import org.bukkit.plugin.java.JavaPlugin;
     @Override
     public void onDisable() {
         getLogger().info("Disabling ShiftPath. If you need me to update this plugin, email at gogobebe2@gmail.com");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("shiftpath") || label.equalsIgnoreCase("sp")) {
+            ItemStack wand = new ItemStack(Material.STICK, 1);
+            ItemMeta wandMeta = wand.getItemMeta();
+            List<String> wandLore = wandMeta.getLore();
+            wandMeta.setDisplayName(ChatColor.BLUE + "" + ChatColor.MAGIC + "[" + ChatColor.LIGHT_PURPLE
+                    + ChatColor.BOLD + "Wand" + ChatColor.BLUE + "" + ChatColor.MAGIC + "]");
+            wandLore.clear();
+            wandLore.add(ChatColor.GREEN + "Left click to define a platform path.");
+            wandLore.add(ChatColor.AQUA + "Right click to set the path defined.");
+            return true;
+        }
+        return false;
     }
 }
