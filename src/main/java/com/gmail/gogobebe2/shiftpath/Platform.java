7adedd71090a4e382210c6f67eebcb4db79d4db8
gogobebe2 <gogobebe2@gmail.com>
2015-06-23 18:50:02 +1200

    1.6.5-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftpath/Platform.java
+++ src/main/java/com/gmail/gogobebe2/shiftpath/Platform.java
@@ -1,5 +1,6 @@
 package com.gmail.gogobebe2.shiftpath;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -11,7 +12,7 @@ public class Platform {
     private Location selection1;
     private Location selection2;
     private Location center;
-    private Set<Block> blocks = new HashSet<>();
+    private Set<Block> structure = new HashSet<>();
 
     public Platform(Location selection1, Location selection2) {
         this.selection1 = selection1;
@@ -20,35 +21,39 @@ public class Platform {
         for (int x = selection1.getBlockX(); x < selection2.getBlockX() + 1; x++) {
             for (int y = selection1.getBlockY(); y < selection2.getBlockY() + 1; y++) {
                 for (int z = selection1.getBlockZ(); z < selection2.getBlockZ() + 1; z++) {
-                    blocks.add(selection1.getWorld().getBlockAt(x, y, z));
+                    structure.add(selection1.getWorld().getBlockAt(x, y, z));
                     centerX += x; centerY += y; centerZ += z;
                 }
             }
         }
-        if (centerX != 0) centerX /= blocks.size();
-        if (centerY != 0) centerY /= blocks.size();
-        if (centerY != 0) centerZ /= blocks.size();
+        if (centerX != 0) centerX /= structure.size();
+        if (centerY != 0) centerY /= structure.size();
+        if (centerY != 0) centerZ /= structure.size();
 
         this.center = selection1.getWorld().getBlockAt(centerX, centerY, centerZ).getLocation();
     }
 
     public void move(Location destination) {
-        // TODO: use in ActivePath.
         int xDistance = destination.getBlockX() - center.getBlockX();
         int yDistance = destination.getBlockY() - center.getBlockY();
         int zDistance = destination.getBlockZ() - center.getBlockZ();
+        Bukkit.broadcastMessage("xDis: " + xDistance);
+        Bukkit.broadcastMessage("yDis: " + yDistance);
+        Bukkit.broadcastMessage("zDis: " + zDistance);
 
-        Set<Block> newBlocks = new HashSet<>();
-        for (Block block : blocks) {
+        Set<Block> blocks = new HashSet<>();
+        for (Block block : structure) {
             Block newBlock = block.getWorld().getBlockAt(block.getLocation().clone().add(xDistance, yDistance, zDistance));
             newBlock.setType(block.getType());
             newBlock.getState().setData(block.getState().getData());
             newBlock.getState().update();
             block.setType(Material.AIR);
-            newBlocks.add(block);
+            block.getState().update();
+            blocks.add(newBlock);
+            blocks.remove(block);
         }
 
-        blocks = newBlocks;
+        structure = blocks;
         center = center.clone().add(xDistance, yDistance, zDistance);
         selection1 = selection1.clone().add(xDistance, yDistance, zDistance);
         selection2 = selection2.clone().add(xDistance, yDistance, zDistance);
